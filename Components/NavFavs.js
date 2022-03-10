01b3fe8cf6938c64b1ffc2f798f89701a91e3bb3
Rishi Raghu <rishiraghu22@yahoo.com>
2022-03-10 13:53:56 +0530

    Map-Directions

---
+++ Components/NavFavs.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NavFavs = () => {
+  return (
+    <View>
+      <Text>NavFavs</Text>
+    </View>
+  )
+}
+
+export default NavFavs
+
+const styles = StyleSheet.create({})

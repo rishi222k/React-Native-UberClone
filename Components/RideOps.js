3afb748c0166235ba9dd2ea6c1444e3a1e993ce9
Rishi Raghu <rishiraghu22@yahoo.com>
2022-03-10 21:06:28 +0530

    Map-Menu Building

--- Components/RideOps.js
+++ Components/RideOps.js
@@ -1,14 +1,94 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View,SafeAreaView,TouchableOpacity,Image } from 'react-native'
+import {useFonts,Inter_300Light,Inter_400Regular,Inter_500Medium,Inter_600SemiBold,Inter_700Bold,
+} from '@expo-google-fonts/inter';
+import { Icon } from 'react-native-elements';
+import React, { useState } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { FlatList } from 'react-native-gesture-handler';
+import tw from 'twrnc';
 
 const RideOps = () => {
+  const navigation= useNavigation();
+  const [selected,setSelected] = useState(null);
+
+  const data =[
+    {
+      id:"Uber-X-123",
+      title:"Uber-X",
+      image: require('../Images/Primarycar.png'),
+      multiplier:1,
+    },
+    {
+      id:"Uber-XL-456",
+      title:"Uber-XL",
+      image: require('../Images/UberXL.png'),
+      multiplier:1.2,
+    },
+    {
+      id:"Uber-LUX-789",
+      title:"Uber-LUX",
+      image: require('../Images/Lux.png'),
+      multiplier:1.75,
+    },
+  ];
+
   return (
+    <SafeAreaView style={{backgroundColor:"#fff",flex:1,}}>
+    <View style={{paddingHorizontal:'3%',}}>
     <View>
-      <Text>RideOps</Text>
+      <TouchableOpacity 
+      style={{position:'absolute',top:25,left:10,zIndex:50}}
+      onPress={() => navigation.navigate("NavigateCard")}>
+      <Icon 
+        name='arrow-left'
+        type='font-awesome-5'
+        color='#000'
+        size={20}
+      />
+      </TouchableOpacity>
+      <Text style={{textAlign:"center",fontSize:20,fontFamily:'Inter_600SemiBold',paddingVertical:20}}>
+      Select a Ride</Text>
     </View>
+    <FlatList 
+      data={data}
+      keyExtractor={(item)=> item.id}
+      renderItem={({ item:{id,title,multiplier,image}, item }) => (
+        <TouchableOpacity 
+        onPress={()=> setSelected(item)}
+        style={tw`flex-row justify-between items-center px-5 rounded ${id === selected?.id && "bg-gray-200"}`}>
+          <Image 
+          style={{width:80,height:90,resizeMode:"contain"}}
+          source={image}
+          />
+          <View>
+          <Text style={{fontSize:20,fontFamily:'Inter_600SemiBold'}}>{title}</Text>
+          <Text>Travel Time...</Text>
+          </View>
+          <Text style={{fontSize:20,fontFamily:'Inter_500Medium'}}>$99</Text>
+        </TouchableOpacity>
+      )}
+    />
+    <View>
+      <TouchableOpacity 
+      disabled={!selected}
+      style={[styles.choose,selected? {backgroundColor:"black"}:{backgroundColor:"#CACACA"}]}>
+        <Text style={{textAlign:'center',color:'white',fontSize:20}}>
+          Choose {selected?.title}
+        </Text>
+      </TouchableOpacity>
+    </View>
+
+    </View>
+    </SafeAreaView>
   )
 }
 
 export default RideOps
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  choose:{
+  paddingHorizontal:25,
+  paddingVertical:15,
+   borderRadius:10
+  },
+})
